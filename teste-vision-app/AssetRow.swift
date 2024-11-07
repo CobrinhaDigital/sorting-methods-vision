cbb68e1adb40cdf0ec6aa9d7ffd36ef6c62529c3
Beatriz <beatrizfpeixoto@gmail.com>
2024-11-07 11:15:53 -0300

    feature: randomizacao das fileiras ok

--- teste-vision-app/AssetRow.swift
+++ teste-vision-app/AssetRow.swift
@@ -12,12 +12,14 @@ import RealityKitContent
 struct AssetRowView: View {
     @State var rowNum: Int
     
-//    let randomNum = []
+    let randomNum = (0..<10).map {
+        _ in Int.random(in: 0..<10)
+    }
     
     var body: some View {
         HStack {
-            ForEach(0...rowNum, id: \.self) { index in
-                AssetView(sphereNum: index)
+            ForEach(0..<randomNum.count, id: \.self) { index in
+                AssetView(sphereNum: randomNum[index])
             }
         }
     }
